8e3f0d786f2950a93d93ce0074ebca6a38a505b9
simar-singh <simar.singh.r@gmail.com>
2021-10-11 18:59:27 -0400

    Added PWA Integration
    Added all of the PWA integrations using the site itself to make the web app a full PWA.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,6 +7,7 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <ModelProvider>
       <SubmissionProvider>
+        <meta name='viewport' content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover' />
         <Component {...pageProps} />
       </SubmissionProvider>
     </ModelProvider>
---
+++ pages/_document.tsx
@@ -0,0 +1,31 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
+  }
+
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <title>PWA Generator</title>
+          <meta name='application-name' content='PWA App' />
+          <meta name='apple-mobile-web-app-capable' content='yes' />
+          <meta name='apple-mobile-web-app-status-bar-style' content='default' />
+          <meta name='apple-mobile-web-app-title' content='PWA App' />
+
+          <link rel='icon' type='image/png' href='/icons/icon.png' />
+          <link rel='manifest' href='/manifest.json' />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
